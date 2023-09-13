431bfafd2e5ac28190016afa57bc67f7d2371145
Drew Young <dmy625@comcast.net>
2023-09-12 20:08:58 -0400

    added robots maybe

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY app.py .
 COPY Young_Drew_Resume.pdf .
+COPY robots.txt .
 ENTRYPOINT ["flask", "run", "--host=0.0.0.0", "--port=8080"]
--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app = flask.Flask(__name__)
 def resume():
 	return flask.send_file('Young_Drew_Resume.pdf')
 
+@app.route("/robots.txt")
+def robots():
+	return flask.send_file("robots.txt")
+
 app.run()
